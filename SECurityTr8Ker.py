d53ee260b62e0b079fe397f4c61fd2f2641ea33c
pancak3lullz <pancak3lullz@gmail.com>
2024-02-07 09:02:54 -0600

    Update SECurityTr8Ker.py
    too many false positives for my liking with the additional search terms.

--- SECurityTr8Ker.py
+++ SECurityTr8Ker.py
@@ -61,7 +61,7 @@ def get_ticker_symbol(cik_number, company_name):
 def inspect_document_for_cybersecurity(link):
     headers = {'User-Agent': 'Mozilla/5.0'}
     # Define a list of search terms you're interested in
-    search_terms = ["Material Cybersecurity Incidents", "cyber attack", "cybersecurity incident"]
+    search_terms = ["Material Cybersecurity Incidents"]
     try:
         response = requests.get(link, headers=headers)
         time.sleep(REQUEST_INTERVAL)
